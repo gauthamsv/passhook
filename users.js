1b41f13a369e81f73219acb632b8268ea0e06d57
gauthamsv <gauthamsv1995@gmail.com>
2021-12-21 12:06:04 +0530

    Create users.js

---
+++ users.js
@@ -0,0 +1,23 @@
+const users = [
+    {
+      username: 'michelletest@example.com',
+      password: 'hunter2',
+    },
+    {
+      username: 'jennifer.cleversy@example.com',
+      password: 'password1',
+    },
+      {
+      username: 'dave.williams@example.com',
+      password: 'password2',
+    },
+  ]
+  
+  
+  
+  function validate(username, password) {
+    return users.some(user => user.username === username && user.password === password);
+    
+    
+  }
+  exports.validate = validate;
